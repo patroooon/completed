1dba943c1ba5b535b278600233f04ff555ecddba
Pavel Home <kapabahone@gmail.com>
2023-10-16 22:59:46 +0300

    1610.2

---
+++ Assets/Scripts/HitCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Golf
+{
+    public class HitCounter : MonoBehaviour
+    {
+        private int hitCount = 0;
+
+        private void Start()
+        {
+            Target target = GetComponent<Target>();
+            target.OnHit += IncrementHitCount;
+        }
+
+        private void IncrementHitCount()
+        {
+            hitCount++;
+            Debug.Log("—чет: " + hitCount);
+        }
+
+    }
+}
--- Assets/Scripts/test/GameController.cs
+++ Assets/Scripts/test/GameController.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace TestXlab
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameController : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
